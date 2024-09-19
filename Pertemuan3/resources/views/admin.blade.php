32a5952c675740aeb4cd4ceb6dcae9318691623b
ririnmedis <rmedistarani@gmail.com>
2024-09-19 18:48:14 +0700

    Pertemuan3

---
+++ Pertemuan3/resources/views/admin.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>admin</title>
+</head>
+<body>
+    <!-- resources/views/admin/profile.blade.php -->
+<h1>Admin Profile</h1>
+<p>Ini adalah halaman profil admin.</p>
+
+</body>
+</html>
---
+++ Pertemuan3/resources/views/user.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Info</title>
+</head>
+<body>
+    <h1>User ID: {{ $id }}</h1>
+</body>
+</html>
