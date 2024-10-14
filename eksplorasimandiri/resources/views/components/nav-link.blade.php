085aaf184a3409f97b5545765d8f81be70bf55b0
ririnmedis <rmedistarani@gmail.com>
2024-10-15 05:01:08 +0700

    Pertemuan2

--- eksplorasimandiri/resources/views/components/nav-link.blade.php
+++
@@ -1,3 +0,0 @@
-<a {{ $attributes}}
-class="{{ request()->is('/home')? ' bg-gray-900 px-3 py-2 text-sm font-medium text-white': 'hover:bg-gray-700 hover:text-white'}}
-              rounded-md" aria-current="{{request()->is('/home')? 'page' : false}}">{{$slot}}</a>
